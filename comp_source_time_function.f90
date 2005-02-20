96ec30e08ba1b51ccf3cd679ede289f547da7437
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2005-02-20 01:33:50 +0000

    fixed small details in two comments

--- comp_source_time_function.f90
+++ comp_source_time_function.f90
@@ -1,11 +1,11 @@
 !=====================================================================
 !
-!          S p e c f e m 3 D  G l o b e  V e r s i o n  3 . 5
+!          S p e c f e m 3 D  B a s i n  V e r s i o n  1 . 2
 !          --------------------------------------------------
 !
 !                 Dimitri Komatitsch and Jeroen Tromp
 !    Seismological Laboratory - California Institute of Technology
-!        (c) California Institute of Technology July 2004
+!         (c) California Institute of Technology July 2004
 !
 !    A signed non-commercial agreement is required to use this program.
 !   Please check http://www.gps.caltech.edu/research/jtromp for details.
