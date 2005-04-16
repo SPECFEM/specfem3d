452cb3bf81575bc27aac0bb1917b5b2fc0f75f04
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2005-04-16 22:07:53 +0000

    fixed portability problem that made reading of Par_file crash on IBM Power4

--- read_value_parameters.f90
+++ read_value_parameters.f90
@@ -67,7 +67,7 @@
   character(len=100) string_read
 
   call read_next_line(string_read)
-  read(string_read,"(a)") value_to_read
+  value_to_read = string_read
 
   end subroutine read_value_string
 
