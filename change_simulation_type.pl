229160d45a08c8f895fa45e5ba1c8d4292ca22dc
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2006-03-03 15:13:50 +0000

    modified script to use DATA/Par_file now instead of values_from_mesher.h

--- change_simulation_type.pl
+++ change_simulation_type.pl
@@ -8,7 +8,7 @@ sub Usage{
 print STDERR <<END;
 
 Usage:   change_simulation_type.pl  [-a|-f|-b|-F]
-         Changes SIMULATION_TYPE in OUTPUT_FILES/values_from_mesher.h
+         Changes SIMULATION_TYPE in DATA/Par_file
          -a -- change type to run adjoint calculation(2)
          -f -- change type to run forward calculation(1)
          -b -- change type to run both simultaneously(3)
@@ -20,42 +20,42 @@ exit(1);
 @ARGV == 1 or Usage();
 if(!getopts('abfF')) {die("check input arguments\n");}
 
-open(IN,"OUTPUT_FILES/values_from_mesher.h");
-@vfm=<IN>;
+open(IN,"DATA/Par_file");
+@file_to_modify=<IN>;
 close(IN);
 
-foreach $vfm (@vfm){
-  if($vfm=~/SIMULATION_TYPE/){
+foreach $file_to_modify (@file_to_modify){
+  if($file_to_modify=~/SIMULATION_TYPE/){
     if(${opt_a}){
-      print "Changed simulation_type to 2 in values_from_mesher.h \n";
-      $vfm=~s/= 1/= 2/;
-      $vfm=~s/= 3/= 2/;
+      print "Changed simulation_type to 2 in Par_file \n";
+      $file_to_modify=~s/= 1/= 2/;
+      $file_to_modify=~s/= 3/= 2/;
     }
     elsif(${opt_f}){
-      print "Changed simulation_type to 1 in values_from_mesher.h \n";
-      $vfm=~s/= 2/= 1/;
-      $vfm=~s/= 3/= 1/;
+      print "Changed simulation_type to 1 in Par_file \n";
+      $file_to_modify=~s/= 2/= 1/;
+      $file_to_modify=~s/= 3/= 1/;
     }
     elsif(${opt_b}){
-      print "Changed simulation_type to 3 in values_from_mesher.h \n";
-      $vfm=~s/= 1/= 3/;
-      $vfm=~s/= 2/= 3/;
+      print "Changed simulation_type to 3 in Par_file \n";
+      $file_to_modify=~s/= 1/= 3/;
+      $file_to_modify=~s/= 2/= 3/;
     }
     elsif(${opt_F}){
-      print "Changed simulation_type to 1 and save_forward = .true. in values_from_mesher.h \n";
-      $vfm=~s/= 2/= 1/;
-      $vfm=~s/= 3/= 1/;
+      print "Changed simulation_type to 1 and save_forward = .true. in Par_file \n";
+      $file_to_modify=~s/= 2/= 1/;
+      $file_to_modify=~s/= 3/= 1/;
     }
   }
-  if ($vfm=~/SAVE_FORWARD/) {
-    if ($opt_F) { $vfm=~s/= .*/= .true./; }
-    else {$vfm=~s/= .*/= .false./;}
+  if ($file_to_modify=~/SAVE_FORWARD/) {
+    if ($opt_F) { $file_to_modify=~s/= .*/= .true./; }
+    else {$file_to_modify=~s/= .*/= .false./;}
 
   }
 }
 
-open(OUT,">OUTPUT_FILES/values_from_mesher.h") || die("No OUTPUT_FILES/values_from_mesher.h\n");
-foreach $vfm (@vfm){
-  print OUT "$vfm";
+open(OUT,">DATA/Par_file") || die("No DATA/Par_file\n");
+foreach $file_to_modify (@file_to_modify){
+  print OUT "$file_to_modify";
 }
 close(OUT);
