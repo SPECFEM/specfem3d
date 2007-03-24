c368f5d78d872d6f850cbe313572905a187d0d97
Qinya Liu <liuqy@physics.utoronto.ca>
2007-03-24 23:32:24 +0000

    add computation of adjoint derivatives of M_jk and source location

--- write_seismograms.f90
+++ write_seismograms.f90
@@ -125,7 +125,7 @@
 
 !=====================================================================
 
-! write adjoint seismograms to text files
+! write adjoint seismograms (displacement) to text files
 
   subroutine write_adj_seismograms(myrank,seismograms,number_receiver_global, &
                nrec_local,it,DT,NSTEP,hdur,LOCAL_PATH,istore)
@@ -221,3 +221,98 @@
 
   end subroutine write_adj_seismograms
 
+!=====================================================================
+
+! write adjoint seismograms (strain) to text files
+
+  subroutine write_adj_seismograms2(myrank,seismograms,number_receiver_global, &
+               nrec_local,it,DT,NSTEP,hdur,LOCAL_PATH)
+
+  implicit none
+
+  include "constants.h"
+
+  integer nrec_local,NSTEP,it,myrank
+  integer, dimension(nrec_local) :: number_receiver_global
+  real(kind=CUSTOM_REAL), dimension(NDIM,NDIM,nrec_local,NSTEP) :: seismograms
+  double precision hdur,DT
+  character(len=150) LOCAL_PATH
+
+
+  integer irec,irec_local
+  integer idim,jdim,irecord,isample
+
+  character(len=4) chn
+  character(len=1) component
+  character(len=150) sisname,clean_LOCAL_PATH,final_LOCAL_PATH
+
+  do irec_local = 1,nrec_local
+
+! get global number of that receiver
+    irec = number_receiver_global(irec_local)
+
+! save three components of displacement vector
+    irecord = 1
+
+    do idim = 1, 3
+      do jdim = idim, 3
+
+      if(idim == 1 .and. jdim == 1) then
+        chn = 'SNN'
+      else if(idim == 1 .and. jdim == 2) then
+        chn = 'SEN'
+      else if(idim == 1 .and. jdim == 3) then
+        chn = 'SEZ'
+      else if(idim == 2 .and. jdim == 2) then
+        chn = 'SEE'
+      else if(idim == 2 .and. jdim == 3) then
+        chn = 'SNZ'
+      else if(idim == 3 .and. jdim == 3) then
+        chn = 'SZZ'
+      else
+        call exit_MPI(myrank,'incorrect channel value')
+      endif
+
+! create the name of the seismogram file for each slice
+! file name includes the name of the station, the network and the component
+
+      write(sisname,"(a,i5.5,'.',a,'.',a3,'.sem',a1)") 'S',irec_local,&
+           'NT',chn,component
+
+! suppress white spaces if any
+    clean_LOCAL_PATH = adjustl(LOCAL_PATH)
+
+! create full final local path
+    final_LOCAL_PATH = clean_LOCAL_PATH(1:len_trim(clean_LOCAL_PATH)) // '/'
+
+! save seismograms in text format with no subsampling.
+! Because we do not subsample the output, this can result in large files
+! if the simulation uses many time steps. However, subsampling the output
+! here would result in a loss of accuracy when one later convolves
+! the results with the source time function
+      open(unit=IOUT,file=final_LOCAL_PATH(1:len_trim(final_LOCAL_PATH))//sisname(1:len_trim(sisname)),status='unknown')
+
+! make sure we never write more than the maximum number of time steps
+! subtract half duration of the source to make sure travel time is correct
+      do isample = 1,min(it,NSTEP)
+        if(irecord == 1) then
+! distinguish between single and double precision for reals
+          if(CUSTOM_REAL == SIZE_REAL) then
+            write(IOUT,*) sngl(dble(isample-1)*DT - hdur),' ',seismograms(jdim,idim,irec_local,isample)
+          else
+            write(IOUT,*) dble(isample-1)*DT - hdur,' ',seismograms(jdim,idim,irec_local,isample)
+          endif
+        else
+          call exit_MPI(myrank,'incorrect record label')
+        endif
+      enddo
+
+      close(IOUT)
+
+    enddo
+
+  enddo
+
+end do
+  
+end subroutine write_adj_seismograms2
