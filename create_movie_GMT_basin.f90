2f2d2ac2ab946fbfddfd22c4a6d19589c146144b
Ying Zhou <yingz@vt.edu>
2007-11-08 23:59:00 +0000

    Change the license to GPL v2+

--- create_movie_GMT_basin.f90
+++ create_movie_GMT_basin.f90
@@ -7,11 +7,19 @@
 !    Seismological Laboratory - California Institute of Technology
 !        (c) California Institute of Technology September 2002
 !
-!    A signed non-commercial agreement is required to use this program.
-!   Please check http://www.gps.caltech.edu/research/jtromp for details.
-!           Free for non-commercial academic research ONLY.
-!      This program is distributed WITHOUT ANY WARRANTY whatsoever.
-!      Do not redistribute this program without written permission.
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 !
 !=====================================================================
 
