a749e08ec40f36b603b90d1fbf224ecffa3b7bc9
Pieyre Le Loher <pieyre.le-loher@inria.fr>
2009-05-26 09:41:14 +0000

    small modifications in memory_eval.f90 and constants.h.in

--- memory_eval.f90
+++ memory_eval.f90
@@ -42,9 +42,6 @@
 ! output
   double precision, intent(out) :: static_memory_size
 
-  integer, parameter :: SIZE_INTEGER = SIZE_REAL
-  integer, parameter :: SIZE_LOGICAL = SIZE_REAL
-
 
   static_memory_size = 0.d0
 
@@ -92,69 +89,3 @@
 
 
 
-
-
-!   subroutine memory_eval(ATTENUATION,NSPEC,nglob,static_memory_size)
-
-!   implicit none
-
-!   include "constants.h"
-
-! ! input
-!   logical, intent(in) :: ATTENUATION
-!   integer, dimension(MAX_NUM_REGIONS), intent(in) :: NSPEC, nglob
-
-! ! output
-!   double precision, intent(out) :: static_memory_size
-
-! ! variables
-!   integer :: NSPEC_INNER_CORE_ATTENUATION,NUMBER_OF_MESH_LAYERS
-
-!   if (ONE_CRUST) then
-!     NUMBER_OF_MESH_LAYERS = MAX_NUMBER_OF_MESH_LAYERS - 1
-!   else
-!   NUMBER_OF_MESH_LAYERS = MAX_NUMBER_OF_MESH_LAYERS
-!   endif
-
-!   if(ATTENUATION) then
-!     NSPEC_INNER_CORE_ATTENUATION = NSPEC(IREGION_INNER_CORE)
-!   else
-!     NSPEC_INNER_CORE_ATTENUATION = 1
-!   endif
-
-! ! add size of each set of static arrays multiplied by the number of such arrays
-
-!   static_memory_size = 0.d0
-
-! ! R_memory_inner_core
-!   static_memory_size = static_memory_size + 5.d0*dble(N_SLS)*dble(NGLLX)* &
-!     dble(NGLLY)*dble(NGLLZ)*NSPEC_INNER_CORE_ATTENUATION*dble(CUSTOM_REAL)
-
-! ! xix_outer_core,xiy_outer_core,xiz_outer_core,
-! ! etax_outer_core,etay_outer_core,etaz_outer_core,
-! ! gammax_outer_core,gammay_outer_core,gammaz_outer_core
-! ! rhostore_outer_core,kappavstore_outer_core
-!   static_memory_size = static_memory_size + dble(NGLLX)*dble(NGLLY)*dble(NGLLZ)*NSPEC(IREGION_OUTER_CORE)*11.d0*dble(CUSTOM_REAL)
-
-! ! ibool_outer_core
-!   static_memory_size = static_memory_size + dble(NGLLX)*dble(NGLLY)*dble(NGLLZ)*NSPEC(IREGION_OUTER_CORE)*dble(SIZE_INTEGER)
-
-! ! xstore_outer_core, ystore_outer_core, zstore_outer_core, rmass_outer_core, displ_outer_core, veloc_outer_core, accel_outer_core
-!   static_memory_size = static_memory_size + NGLOB(IREGION_OUTER_CORE)*7.d0*dble(CUSTOM_REAL)
-
-! ! ibool_inner_core
-!   static_memory_size = static_memory_size + dble(NGLLX)*dble(NGLLY)*dble(NGLLZ)*NSPEC(IREGION_INNER_CORE)*dble(SIZE_INTEGER)
-
-! ! xix_inner_core,xiy_inner_core,xiz_inner_core,
-! ! etax_inner_core,etay_inner_core,etaz_inner_core,
-! ! gammax_inner_core,gammay_inner_core,gammaz_inner_core,
-! ! rhostore_inner_core,kappavstore_inner_core,muvstore_inner_core
-!   static_memory_size = static_memory_size + dble(NGLLX)*dble(NGLLY)*dble(NGLLZ)*NSPEC(IREGION_INNER_CORE)*12.d0*dble(CUSTOM_REAL)
-
-! ! xstore_inner_core,ystore_inner_core,zstore_inner_core,rmass_inner_core
-!   static_memory_size = static_memory_size + NGLOB(IREGION_INNER_CORE)*4.d0*dble(CUSTOM_REAL)
-
-! ! displ_inner_core,veloc_inner_core,accel_inner_core
-!   static_memory_size = static_memory_size + dble(NDIM)*NGLOB(IREGION_INNER_CORE)*3.d0*dble(CUSTOM_REAL)
-
-!   end subroutine memory_eval
