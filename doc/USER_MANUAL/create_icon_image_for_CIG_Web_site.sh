2f2d06512ccb40188239636a683cc0da0e2666b5
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2010-11-22 10:40:52 +0000

    added "-quality 100" to the scripts to create icons of the manual cover for the CIG Web site

--- doc/USER_MANUAL/create_icon_image_for_CIG_Web_site.sh
+++ doc/USER_MANUAL/create_icon_image_for_CIG_Web_site.sh
@@ -2,5 +2,5 @@
 
 # Dimitri Komatitsch, November 2010: use "convert" from the Imagemagick package
 
-convert -resize 38% -colorspace RGB figures/specfem_3d-cover.pdf jpg:specfem_3d-cover_small.jpg
+convert -quality 100 -resize 38% -colorspace RGB figures/specfem_3d-cover.pdf jpg:specfem_3d-cover_small.jpg
 
