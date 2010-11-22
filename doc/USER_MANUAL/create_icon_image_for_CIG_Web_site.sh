d76d812f380995f6b00ba96a0adb1d0d2e0fa267
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2010-11-22 10:34:06 +0000

    added scripts to create icons of the manual cover for the CIG Web site

---
+++ doc/USER_MANUAL/create_icon_image_for_CIG_Web_site.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Dimitri Komatitsch, November 2010: use "convert" from the Imagemagick package
+
+convert -resize 38% -colorspace RGB figures/specfem_3d-cover.pdf jpg:specfem_3d-cover_small.jpg
+
