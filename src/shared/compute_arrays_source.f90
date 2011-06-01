e7a9ef8d22a3b4c24ee7b6e1fdb5d39eeeae5659
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2011-06-01 13:37:21 +0000

    moved src/shared/multiply_arrays_source.f90 to a separate file to avoid subroutine inlining by the compiler inside the same file

--- src/shared/compute_arrays_source.f90
+++ src/shared/compute_arrays_source.f90
@@ -115,55 +115,6 @@
 
   end subroutine compute_arrays_source
 
-!================================================================
-
-! we put these multiplications in a separate routine because otherwise
-! some compilers try to unroll the six loops above and take forever to compile
-  subroutine multiply_arrays_source(sourcearrayd,G11,G12,G13,G21,G22,G23, &
-                  G31,G32,G33,hxis,hpxis,hetas,hpetas,hgammas,hpgammas,k,l,m)
-
-  implicit none
-
-  include "constants.h"
-
-! source arrays
-  double precision, dimension(NDIM,NGLLX,NGLLY,NGLLZ) :: sourcearrayd
-  double precision, dimension(NGLLX,NGLLY,NGLLZ) :: G11,G12,G13,G21,G22,G23,G31,G32,G33
-  double precision, dimension(NGLLX) :: hxis,hpxis
-  double precision, dimension(NGLLY) :: hetas,hpetas
-  double precision, dimension(NGLLZ) :: hgammas,hpgammas
-
-  integer k,l,m
-
-  integer ir,it,iv
-
-  sourcearrayd(:,k,l,m) = ZERO
-
-  do iv=1,NGLLZ
-    do it=1,NGLLY
-      do ir=1,NGLLX
-
-        sourcearrayd(1,k,l,m) = sourcearrayd(1,k,l,m) + hxis(ir)*hetas(it)*hgammas(iv) &
-                           *(G11(ir,it,iv)*hpxis(k)*hetas(l)*hgammas(m) &
-                           +G12(ir,it,iv)*hxis(k)*hpetas(l)*hgammas(m) &
-                           +G13(ir,it,iv)*hxis(k)*hetas(l)*hpgammas(m))
-
-        sourcearrayd(2,k,l,m) = sourcearrayd(2,k,l,m) + hxis(ir)*hetas(it)*hgammas(iv) &
-                           *(G21(ir,it,iv)*hpxis(k)*hetas(l)*hgammas(m) &
-                           +G22(ir,it,iv)*hxis(k)*hpetas(l)*hgammas(m) &
-                           +G23(ir,it,iv)*hxis(k)*hetas(l)*hpgammas(m))
-
-        sourcearrayd(3,k,l,m) = sourcearrayd(3,k,l,m) + hxis(ir)*hetas(it)*hgammas(iv) &
-                           *(G31(ir,it,iv)*hpxis(k)*hetas(l)*hgammas(m) &
-                           +G32(ir,it,iv)*hxis(k)*hpetas(l)*hgammas(m) &
-                           +G33(ir,it,iv)*hxis(k)*hetas(l)*hpgammas(m))
-
-      enddo
-    enddo
-  enddo
-
-  end subroutine multiply_arrays_source
-
 !=============================================================================
 
   subroutine compute_arrays_adjoint_source(myrank, adj_source_file, &
