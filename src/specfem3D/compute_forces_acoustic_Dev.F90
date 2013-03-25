4446b66e6c9ab06f12d109d770c2017b88cd5102
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2013-03-25 21:03:42 +0000

    added a comment about the boundary condition to use on outer edges of PML elements in acoustic (fluid) layers

--- src/specfem3D/compute_forces_acoustic_Dev.F90
+++ src/specfem3D/compute_forces_acoustic_Dev.F90
@@ -298,5 +298,11 @@
 
   enddo ! end of loop over all spectral elements
 
+! The outer boundary condition to use for PML elements in fluid layers is Neumann for the potential
+! because we need Dirichlet conditions for the displacement vector, which means Neumann for the potential.
+! Thus, there is nothing to enforce explicitly here.
+! There is something to enforce explicitly only in the case of elastic elements, for which a Dirichlet
+! condition is needed for the displacement vector, which is the vectorial unknown for these elements.
+
   end subroutine compute_forces_acoustic_Dev
 
