0f49e1f97549263ded1608063ac6d419604caadd
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2013-04-13 17:10:53 +0000

    added integration weight and 1/2 coefficient to utils/compute_expression_of_potential_energy.f90

--- utils/compute_expression_of_potential_energy.f90
+++ utils/compute_expression_of_potential_energy.f90
@@ -9,13 +9,19 @@
 
   integer :: i,j
 
-  print *,'potential_energy = potential_energy + &'
+! compute potential energy 1/2 sigma_ij epsilon_ij
+
+  print *,'potential_energy = potential_energy + integration_weight * 0.5 * ( &'
 
 ! expression in 3D
 ! 1 = x, 2 = y and 3 = z
   do i = 1,3
     do j = 1,3
-      print *,'sigma_',i,j,' * epsilon_',i,j,' + '
+      if(i == 3 .and. j == 3) then
+        print *,'sigma_',i,j,' * epsilon_',i,j,')'
+      else
+        print *,'sigma_',i,j,' * epsilon_',i,j,' + '
+      endif
     enddo
   enddo
 
