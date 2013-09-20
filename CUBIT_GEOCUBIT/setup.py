7df307cff7221e111f0cf02c98b82f6eb394d1fe
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2013-09-20 17:32:11 +0000

    moved the final SVN version of GEOCUBIT at CIG to the SPECFEM3D_Cartesian package, to make it easier to maintain and to avoid having two different versions, none of them working fine in all cases and for all users

---
+++ CUBIT_GEOCUBIT/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import glob
+
+setup(name='GEOCUBIT',
+      version='3',
+      description='CUBIT plugin',
+      author='emanuele casarotti',
+      author_email='emanuele.casarotti@ingv.it',
+      url='',
+      packages=['','geocubitlib'],
+      scripts=['GEOCUBIT.py']
+)
+
