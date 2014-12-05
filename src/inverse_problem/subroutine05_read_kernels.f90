509c631db172c2d85f326dd7e547302d9fb5621a
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2014-12-05 02:18:21 +0100

    added src/inverse_problem (for our work with Vadim Monteiller: as planned we are trying a way of parallelizing the Fortran inversion tools to see how fast they can be made)

---
+++ src/inverse_problem/subroutine05_read_kernels.f90
@@ -0,0 +1,269 @@
+!=====================================================================
+!
+!               S p e c f e m 3 D  V e r s i o n  2 . 1
+!               ---------------------------------------
+!
+!     Main historical authors: Dimitri Komatitsch and Jeroen Tromp
+!                        Princeton University, USA
+!                and CNRS / University of Marseille, France
+!                 (there are currently many more authors!)
+! (c) Princeton University and CNRS / University of Marseille, July 2012
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+!
+!=====================================================================
+
+
+subroutine read_kernels_iso()
+
+! reads in smoothed kernels: bulk, beta, rho
+
+  use inverse_problem_kernels_iso
+
+  implicit none
+  ! local parameters
+  real(kind=CUSTOM_REAL) :: min_vp,min_vs,max_vp,max_vs,min_rho,max_rho
+  integer :: ier
+  character(len=MAX_STRING_LEN) :: m_file, fname
+
+  ! user output
+  if (myrank == 0) print*,'reading kernels...'
+
+  ! allocate arrays for storing kernels and perturbations
+  ! isotropic arrays
+  allocate(kernel_bulk(NGLLX,NGLLY,NGLLZ,NSPEC), &
+           kernel_beta(NGLLX,NGLLY,NGLLZ,NSPEC), &
+           kernel_rho(NGLLX,NGLLY,NGLLZ,NSPEC),stat=ier)
+  if (ier /= 0) stop 'error allocating kernel arrays'
+
+  ! initializes arrays
+  kernel_bulk = 0.0_CUSTOM_REAL
+  kernel_beta = 0.0_CUSTOM_REAL
+  kernel_rho = 0.0_CUSTOM_REAL
+
+  ! reads in smoothed (& summed) event kernel
+  if (USE_ALPHA_BETA_RHO) then
+    ! reads in alpha kernel
+    fname = 'alpha_kernel_smooth'
+  else
+    ! reads in bulk_c kernel
+    fname = 'bulk_c_kernel_smooth'
+  endif
+  write(m_file,'(a,i6.6,a)') trim(INPUT_KERNELS_DIR)//'proc',myrank,trim(REG)//trim(fname)//'.bin'
+  if (myrank == 0) print*,'  ',trim(INPUT_KERNELS_DIR)//'proc**'//trim(REG)//trim(fname)//'.bin'
+
+  open(IIN,file=trim(m_file),status='old',form='unformatted',iostat=ier)
+  if (ier /= 0) then
+    print*,'Error opening: ',trim(m_file)
+    call exit_mpi(myrank,'file not found')
+  endif
+  read(IIN) kernel_bulk(:,:,:,1:nspec)
+  close(IIN)
+
+  ! beta kernel
+  if (USE_ALPHA_BETA_RHO) then
+    ! reads in beta kernel
+    fname = 'beta_kernel_smooth'
+  else
+    ! reads in bulk_beta kernel
+    fname = 'bulk_beta_kernel_smooth'
+  endif
+  write(m_file,'(a,i6.6,a)') trim(INPUT_KERNELS_DIR)//'proc',myrank,trim(REG)//trim(fname)//'.bin'
+  if (myrank == 0) print*,'  ',trim(INPUT_KERNELS_DIR)//'proc**'//trim(REG)//trim(fname)//'.bin'
+
+  open(IIN,file=trim(m_file),status='old',form='unformatted',iostat=ier)
+  if (ier /= 0) then
+    print*,'Error opening: ',trim(m_file)
+    call exit_mpi(myrank,'file not found')
+  endif
+  read(IIN) kernel_beta(:,:,:,1:nspec)
+  close(IIN)
+
+  ! rho kernel
+  if (USE_RHO_SCALING) then
+    ! uses scaling relation with shear perturbations
+    kernel_rho(:,:,:,:) = RHO_SCALING * kernel_beta(:,:,:,:)
+    if (myrank == 0) print*,'  rho kernel uses scaling with shear kernel: scaling value = ',RHO_SCALING
+  else
+    ! uses rho kernel
+    fname = 'rho_kernel_smooth'
+    write(m_file,'(a,i6.6,a)') trim(INPUT_KERNELS_DIR)//'proc',myrank,trim(REG)//trim(fname)//'.bin'
+    if (myrank == 0) print*,'  ',trim(INPUT_KERNELS_DIR)//'proc**'//trim(REG)//trim(fname)//'.bin'
+
+    open(IIN,file=trim(m_file),status='old',form='unformatted',iostat=ier)
+    if (ier /= 0) then
+      print*,'Error opening: ',trim(m_file)
+      call exit_mpi(myrank,'file not found')
+    endif
+    read(IIN) kernel_rho(:,:,:,1:nspec)
+    close(IIN)
+  endif
+
+  ! statistics
+  call min_all_cr(minval(kernel_bulk),min_vp)
+  call max_all_cr(maxval(kernel_bulk),max_vp)
+
+  call min_all_cr(minval(kernel_beta),min_vs)
+  call max_all_cr(maxval(kernel_beta),max_vs)
+
+  call min_all_cr(minval(kernel_rho),min_rho)
+  call max_all_cr(maxval(kernel_rho),max_rho)
+
+  if (myrank == 0) then
+    print*
+    print*,'initial kernels:'
+    if (USE_ALPHA_BETA_RHO) then
+      print*,'  alpha min/max    : ',min_vp,max_vp
+      print*,'  beta min/max     : ',min_vs,max_vs
+    else
+      print*,'  bulk_c min/max   : ',min_vp,max_vp
+      print*,'  bulk_beta min/max: ',min_vs,max_vs
+    endif
+    print*,'  rho min/max      : ',min_rho,max_rho
+    print*
+  endif
+  call synchronize_all()
+
+  ! statistics output
+  if (PRINT_STATISTICS_FILES .and. myrank == 0) then
+    open(IOUT,file=trim(OUTPUT_STATISTICS_DIR)//'statistics_kernels_minmax',status='unknown')
+    write(IOUT,*) '#min_vs #max_vs #min_vp #max_vp #min_rho #max_rho'
+    write(IOUT,'(4e24.12)') min_vs, max_vs, min_vp, max_vp, min_rho, max_rho
+    close(IOUT)
+  endif
+
+end subroutine read_kernels_iso
+
+!
+!-------------------------------------------------------------------------------------------------
+!
+
+
+subroutine read_kernels_tiso()
+
+! reads in smoothed kernels: bulk, betav, betah, eta
+
+  use inverse_problem_kernels_tiso
+
+  implicit none
+  ! local parameters
+  real(kind=CUSTOM_REAL) :: min_vsv,min_vsh,max_vsv,max_vsh,min_eta,max_eta,min_bulk,max_bulk
+  integer :: ier
+  character(len=MAX_STRING_LEN) :: m_file, fname
+
+  ! user output
+  if (myrank == 0) print*,'reading kernels...'
+
+  ! allocate arrays for storing kernels and perturbations
+  ! transversely isotropic arrays
+  allocate(kernel_bulk(NGLLX,NGLLY,NGLLZ,NSPEC), &
+           kernel_betav(NGLLX,NGLLY,NGLLZ,NSPEC), &
+           kernel_betah(NGLLX,NGLLY,NGLLZ,NSPEC), &
+           kernel_eta(NGLLX,NGLLY,NGLLZ,NSPEC),stat=ier)
+  if (ier /= 0) stop 'error allocating kernel arrays'
+
+  ! initializes arrays
+  kernel_bulk = 0.0_CUSTOM_REAL
+  kernel_betav = 0.0_CUSTOM_REAL
+  kernel_betah = 0.0_CUSTOM_REAL
+  kernel_eta = 0.0_CUSTOM_REAL
+
+  ! bulk kernel
+  fname = 'bulk_c_kernel_smooth'
+  write(m_file,'(a,i6.6,a)') trim(INPUT_KERNELS_DIR)//'proc',myrank,trim(REG)//trim(fname)//'.bin'
+  if (myrank == 0) print*,'  ',trim(INPUT_KERNELS_DIR)//'proc**'//trim(REG)//trim(fname)//'.bin'
+
+  open(IIN,file=trim(m_file),status='old',form='unformatted',iostat=ier)
+  if (ier /= 0) then
+    print*,'Error opening: ',trim(m_file)
+    call exit_mpi(myrank,'file not found')
+  endif
+  read(IIN) kernel_bulk(:,:,:,1:nspec)
+  close(IIN)
+
+  ! betav kernel
+  fname = 'bulk_betav_kernel_smooth'
+  write(m_file,'(a,i6.6,a)') trim(INPUT_KERNELS_DIR)//'proc',myrank,trim(REG)//trim(fname)//'.bin'
+  if (myrank == 0) print*,'  ',trim(INPUT_KERNELS_DIR)//'proc**'//trim(REG)//trim(fname)//'.bin'
+
+  open(IIN,file=trim(m_file),status='old',form='unformatted',iostat=ier)
+  if (ier /= 0) then
+    print*,'Error opening: ',trim(m_file)
+    call exit_mpi(myrank,'file not found')
+  endif
+  read(IIN) kernel_betav(:,:,:,1:nspec)
+  close(IIN)
+
+  ! betah kernel
+  fname = 'bulk_betah_kernel_smooth'
+  write(m_file,'(a,i6.6,a)') trim(INPUT_KERNELS_DIR)//'proc',myrank,trim(REG)//trim(fname)//'.bin'
+  if (myrank == 0) print*,'  ',trim(INPUT_KERNELS_DIR)//'proc**'//trim(REG)//trim(fname)//'.bin'
+
+  open(IIN,file=trim(m_file),status='old',form='unformatted',iostat=ier)
+  if (ier /= 0) then
+    print*,'Error opening: ',trim(m_file)
+    call exit_mpi(myrank,'file not found')
+  endif
+  read(IIN) kernel_betah(:,:,:,1:nspec)
+  close(IIN)
+
+  ! eta kernel
+  fname = 'eta_kernel_smooth'
+  write(m_file,'(a,i6.6,a)') trim(INPUT_KERNELS_DIR)//'proc',myrank,trim(REG)//trim(fname)//'.bin'
+  if (myrank == 0) print*,'  ',trim(INPUT_KERNELS_DIR)//'proc**'//trim(REG)//trim(fname)//'.bin'
+
+  open(IIN,file=trim(m_file),status='old',form='unformatted',iostat=ier)
+  if (ier /= 0) then
+    print*,'Error opening: ',trim(m_file)
+    call exit_mpi(myrank,'file not found')
+  endif
+  read(IIN) kernel_eta(:,:,:,1:nspec)
+  close(IIN)
+
+
+  ! statistics
+  call min_all_cr(minval(kernel_bulk),min_bulk)
+  call max_all_cr(maxval(kernel_bulk),max_bulk)
+
+  call min_all_cr(minval(kernel_betah),min_vsh)
+  call max_all_cr(maxval(kernel_betah),max_vsh)
+
+  call min_all_cr(minval(kernel_betav),min_vsv)
+  call max_all_cr(maxval(kernel_betav),max_vsv)
+
+  call min_all_cr(minval(kernel_eta),min_eta)
+  call max_all_cr(maxval(kernel_eta),max_eta)
+
+  if (myrank == 0) then
+    print*
+    print*,'initial kernels:'
+    print*,'  bulk min/max : ',min_bulk,max_bulk
+    print*,'  betav min/max: ',min_vsv,max_vsv
+    print*,'  betah min/max: ',min_vsh,max_vsh
+    print*,'  eta min/max  : ',min_eta,max_eta
+    print*
+  endif
+  call synchronize_all()
+
+  ! statistics output
+  if (PRINT_STATISTICS_FILES .and. myrank == 0) then
+    open(IOUT,file=trim(OUTPUT_STATISTICS_DIR)//'statistics_kernels_minmax',status='unknown')
+    write(IOUT,*) '#min_bulk #max_bulk #min_vsv #max_vsv #min_vsh #max_vsh #min_eta #max_eta'
+    write(IOUT,'(4e24.12)') min_bulk, max_bulk, min_vsv, max_vsv, min_vsh, max_vsh, min_eta, max_eta
+    close(IOUT)
+  endif
+
+end subroutine read_kernels_tiso
+
---
+++ src/inverse_problem/subroutine11_write_new_model_perturbations.f90
@@ -0,0 +1,231 @@
+!=====================================================================
+!
+!               S p e c f e m 3 D  V e r s i o n  2 . 1
+!               ---------------------------------------
+!
+!     Main historical authors: Dimitri Komatitsch and Jeroen Tromp
+!                        Princeton University, USA
+!                and CNRS / University of Marseille, France
+!                 (there are currently many more authors!)
+! (c) Princeton University and CNRS / University of Marseille, July 2012
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License along
+! with this program; if not, write to the Free Software Foundation, Inc.,
+! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+!
+!=====================================================================
+
+
+subroutine write_new_model_perturbations_iso()
+
+! file output for new model perturbations
+
+  use inverse_problem_model_iso
+
+  implicit none
+  real(kind=CUSTOM_REAL) :: min_vp,min_vs,max_vp,max_vs,min_rho,max_rho
+  real(kind=CUSTOM_REAL), dimension(NGLLX,NGLLY,NGLLZ,NSPEC) :: total_model
+  character(len=MAX_STRING_LEN) :: m_file
+
+  ! user output
+  if (myrank == 0) print*,'writing out model perturbations...'
+
+  ! vp relative perturbations
+  ! logarithmic perturbation: log( v_new) - log( v_old) = log( v_new / v_old )
+  total_model = 0.0_CUSTOM_REAL
+  where( model_vp /= 0.0 ) total_model = log( model_vp_new / model_vp)
+  ! or
+  ! linear approximation: (v_new - v_old) / v_old
+  !where( model_vp /= 0.0 ) total_model = ( model_vp_new - model_vp) / model_vp
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'dvpvp.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'dvpvp.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_vp)
+  call min_all_cr(minval(total_model),min_vp)
+
+  ! vs relative perturbations
+  total_model = 0.0_CUSTOM_REAL
+  where( model_vs /= 0.0 ) total_model = log( model_vs_new / model_vs)
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'dvsvs.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'dvsvs.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_vs)
+  call min_all_cr(minval(total_model),min_vs)
+
+  ! rho relative model perturbations
+  total_model = 0.0_CUSTOM_REAL
+  where( model_rho /= 0.0 ) total_model = log( model_rho_new / model_rho)
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'drhorho.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'drhorho.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_rho)
+  call min_all_cr(minval(total_model),min_rho)
+
+  if (myrank == 0) then
+    print*
+    print*,'relative update:'
+    print*,'  dvp/vp min/max  : ',min_vp,max_vp
+    print*,'  dvs/vs min/max  : ',min_vs,max_vs
+    print*,'  drho/rho min/max: ',min_rho,max_rho
+    print*
+  endif
+  call synchronize_all()
+
+  ! statistics output
+  if (PRINT_STATISTICS_FILES .and. myrank == 0) then
+    open(IOUT,file=trim(OUTPUT_STATISTICS_DIR)//'statistics_relative_vs_vp_rho',status='unknown')
+    write(IOUT,*) '#min_vs #max_vs #min_vp #max_vp #min_rho #max_rho'
+    write(IOUT,'(6e24.12)') min_vs,max_vs,min_vp,max_vp,min_rho,max_rho
+    close(IOUT)
+  endif
+
+end subroutine write_new_model_perturbations_iso
+
+!
+!-------------------------------------------------------------------------------------------------
+!
+
+subroutine write_new_model_perturbations_tiso()
+
+! file output for TI new model perturbations
+
+  use inverse_problem_model_tiso
+  implicit none
+  real(kind=CUSTOM_REAL) :: min_vpv,min_vph,min_vsv,min_vsh, &
+    max_vpv,max_vph,max_vsv,max_vsh,min_eta,max_eta,min_rho,max_rho
+  real(kind=CUSTOM_REAL), dimension(NGLLX,NGLLY,NGLLZ,NSPEC) :: total_model
+
+  character(len=MAX_STRING_LEN) :: m_file
+
+  ! user output
+  if (myrank == 0) print*,'writing out model perturbations...'
+
+  ! vpv relative perturbations
+  ! logarithmic perturbation: log( v_new) - log( v_old) = log( v_new / v_old )
+  total_model = 0.0_CUSTOM_REAL
+  where( model_vpv /= 0.0 ) total_model = log( model_vpv_new / model_vpv)
+  ! or
+  ! linear approximation: (v_new - v_old) / v_old
+  !where( model_vpv /= 0.0 ) total_model = ( model_vpv_new - model_vpv) / model_vpv
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'dvpvvpv.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'dvpvvpv.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_vpv)
+  call min_all_cr(minval(total_model),min_vpv)
+
+  ! vph relative perturbations
+  total_model = 0.0_CUSTOM_REAL
+  where( model_vph /= 0.0 ) total_model = log( model_vph_new / model_vph)
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'dvphvph.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'dvphvph.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_vph)
+  call min_all_cr(minval(total_model),min_vph)
+
+  ! vsv relative perturbations
+  total_model = 0.0_CUSTOM_REAL
+  where( model_vsv /= 0.0 ) total_model = log( model_vsv_new / model_vsv)
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'dvsvvsv.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'dvsvvsv.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_vsv)
+  call min_all_cr(minval(total_model),min_vsv)
+
+  ! vsh relative perturbations
+  total_model = 0.0_CUSTOM_REAL
+  where( model_vsh /= 0.0 ) total_model = log( model_vsh_new / model_vsh)
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'dvshvsh.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'dvshvsh.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_vsh)
+  call min_all_cr(minval(total_model),min_vsh)
+
+  ! eta relative perturbations
+  total_model = 0.0_CUSTOM_REAL
+  where( model_eta /= 0.0 ) total_model = log( model_eta_new / model_eta)
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'detaeta.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'detaeta.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_eta)
+  call min_all_cr(minval(total_model),min_eta)
+
+  ! rho relative model perturbations
+  total_model = 0.0_CUSTOM_REAL
+  where( model_rho /= 0.0 ) total_model = log( model_rho_new / model_rho)
+
+  write(m_file,'(a,i6.6,a)') trim(OUTPUT_MODEL_DIR)//'proc',myrank,trim(REG)//'drhorho.bin'
+  if (myrank == 0) print*,'  ',trim(OUTPUT_MODEL_DIR)//'proc**'//trim(REG)//'drhorho.bin'
+
+  open(IOUT,file=trim(m_file),form='unformatted',action='write')
+  write(IOUT) total_model
+  close(IOUT)
+  call max_all_cr(maxval(total_model),max_rho)
+  call min_all_cr(minval(total_model),min_rho)
+
+  if (myrank == 0) then
+    print*
+    print*,'relative update:'
+    print*,'  dvpv/vpv min/max: ',min_vpv,max_vpv
+    print*,'  dvph/vph min/max: ',min_vph,max_vph
+    print*,'  dvsv/vsv min/max: ',min_vsv,max_vsv
+    print*,'  dvsh/vsh min/max: ',min_vsh,max_vsh
+    print*,'  deta/eta min/max: ',min_eta,max_eta
+    print*,'  drho/rho min/max: ',min_rho,max_rho
+    print*
+  endif
+  call synchronize_all()
+
+  ! statistics output
+  if (PRINT_STATISTICS_FILES .and. myrank == 0) then
+    open(IOUT,file=trim(OUTPUT_STATISTICS_DIR)//'statistics_relative_vs_vp_rho',status='unknown')
+    write(IOUT,*) '#min_vsv #max_vsv #min_vsh #max_vsh #min_vpv #max_vpv #min_vph #max_vph ' &
+               // '#min_eta #max_eta #min_rho #max_rho'
+    write(IOUT,'(12e24.12)') min_vsv,max_vsv,min_vsh,max_vsh,min_vpv,max_vpv,min_vph,max_vph, &
+                            min_eta,max_eta,min_rho,max_rho
+    close(IOUT)
+  endif
+
+end subroutine write_new_model_perturbations_tiso
+
