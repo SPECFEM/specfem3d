762b68989ea35516b8100bcbf48075487525dd17
Clément Durochat <c.durochat@gmail.com>
2015-01-14 19:11:02 +0100

    Modifs directories and install in DSM

--- utils/DSM_FOR_SPECFEM3D/clean.sh
+++ utils/DSM_FOR_SPECFEM3D/clean.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-cd Part1_create_SPECFEM3D_Cartesian_mesh_for_DSM;make clean;cd ..
-cd Part2_compute_DSM_coefficients_with_FEMs_SH; make clean;cd ../
-cd Part2_compute_DSM_coefficients_with_FEMs_PSV; make clean;cd ../
-cd Part3_read_DSM_coefficients_back_SH/TraPSV_MPI_read_vertical_faces; make clean;cd ..
-cd TraPSV_MPI_read_Zmin; make clean;cd ../..
-cd Part3_read_DSM_coefficients_back_PSV/TraPSV_MPI_read_vertical_faces; make clean;cd ..
-cd TraPSV_MPI_read_Zmin; make clean;cd ../..
-cd Part4_modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_SH; make clean;cd ..
-cd FFT_MPI_FACES_VERT_SH; make clean;cd ..
-cd FFT_MPI_FACES_ZMIN_PSV; make clean;cd ..
-cd FFT_MPI_FACES_VERT_PSV; make clean;cd ..
-cd FFT_MPI_FACES_ZMIN_FULL; make clean;cd ..
-cd FFT_MPI_FACES_VERT_FULL; make clean;cd ..
-cd ChangeFormat_zmin_disp; make clean;cd ..
-cd ChangeFormat_zmin; make clean;cd ..
-cd ChangeFormat_disp; make clean;cd ..
-cd ChangeFormat; make clean;cd ..
-cd Interf_SPECFEM3D_DSM;make clean;cd ../..
-cd UTILS;make clean;cd ..
+rm *~
+
+echo " "
+echo "Cleaning from the HOME_DSM_MAIN_DIR directory"
+echo " "
+
+cd Part-1a-compute_DSM_coefficients_with_FEMs_SH/ ; rm *~ ; make clean
+
+cd Part-1b-compute_DSM_coefficients_with_FEMs_PSV/ ; rm *~ ; make clean
+
+cd Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_vertical_faces/ ; rm *~ ; make clean
+cd Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_Zmin/ ; rm *~ ; make clean
+
+cd Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_vertical_faces/ ; rm *~ ; make clean
+cd Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_Zmin/ ; rm *~ ; make clean
+
+cd Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_SH ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_SH ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_PSV ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_PSV ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_FULL ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_FULL ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin_disp ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_disp ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat ; rm *~ ; make clean
+cd Part-3-modify_DSM_results_for_SPECFEM/Interf_SPECFEM3D_DSM ; rm *~ ; make clean
+
+cd UTILS/ ; rm *~ ; make clean 
+
+cd ..
+rm bin/*
 
--- utils/DSM_FOR_SPECFEM3D/install.sh
+++ utils/DSM_FOR_SPECFEM3D/install.sh
@@ -1,24 +1,54 @@
 #!/bin/bash
 
+vert='\e[0;32m'
+blanc='\e[0;37m'
+jaune='\e[1;33m'
+neutre='\e[0;m'
+
+echo " "
+
+HOME_DSM_MAIN_DIR=/ccc/scratch/cont003/gen7165/durochtc/Codes/SPECFEM3Ds/specfem3d/utils/DSM_FOR_SPECFEM3D
+export HOME_DSM_MAIN_DIR
+
+echo -e "!! CAUTION !! Verify the definition of your ${jaune}\033[1mHOME_DSM_MAIN_DIR\033[0m${neutre} in install.sh, currently defined as: "
+echo " "
+echo -e "${jaune}\033[1m${HOME_DSM_MAIN_DIR}\033[0m${neutre}"
+
+echo " "
+echo "The file install.sh is written for the DSM 3D big storage version"
+
 mkdir -p bin
 
-###cd Part1_create_SPECFEM3D_Cartesian_mesh_for_DSM;make;cd ..
-cd Part2_compute_DSM_coefficients_with_FEMs_SH; make;cd ../
-cd Part2_compute_DSM_coefficients_with_FEMs_PSV; make;cd ../
-cd Part3_read_DSM_coefficients_back_SH/TraPSV_MPI_read_vertical_faces; make;cd ..
-cd TraPSV_MPI_read_Zmin; make;cd ../..
-cd Part3_read_DSM_coefficients_back_PSV/TraPSV_MPI_read_vertical_faces; make;cd ..
-cd TraPSV_MPI_read_Zmin; make;cd ../..
-cd Part4_modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_SH; make;cd ..
-cd FFT_MPI_FACES_VERT_SH; make;cd ..
-cd FFT_MPI_FACES_ZMIN_PSV; make;cd ..
-cd FFT_MPI_FACES_VERT_PSV; make;cd ..
-cd FFT_MPI_FACES_ZMIN_FULL; make;cd ..
-cd FFT_MPI_FACES_VERT_FULL; make;cd ..
-cd ChangeFormat_zmin_disp; make;cd ..
-cd ChangeFormat_zmin; make;cd ..
-cd ChangeFormat_disp; make;cd ..
-cd ChangeFormat; make;cd ..
-cd Interf_SPECFEM3D_DSM;make;cd ../..
-cd UTILS;make;cd ..
+echo " "
+echo "============================================================================================================================"
+echo "============================================================================================================================"
+echo " "
+echo "The mesher part is now generated in meshfem3d (as a particular case)"
+
+echo " "
+cd ${HOME_DSM_MAIN_DIR}/Part-1a-compute_DSM_coefficients_with_FEMs_SH/ ; make
+
+cd ${HOME_DSM_MAIN_DIR}/Part-1b-compute_DSM_coefficients_with_FEMs_PSV/ ; make
+
+cd ${HOME_DSM_MAIN_DIR}/Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_vertical_faces/ ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_Zmin/ ; make
+
+cd ${HOME_DSM_MAIN_DIR}/Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_vertical_faces/ ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_Zmin/ ; make
+
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_SH ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_SH ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_PSV ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_PSV ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_FULL ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_FULL ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin_disp ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_disp ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat ; make
+cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/Interf_SPECFEM3D_DSM ; make
+
+cd ${HOME_DSM_MAIN_DIR}/UTILS/ ; make 
+
+cd ${HOME_DSM_MAIN_DIR}/
 
