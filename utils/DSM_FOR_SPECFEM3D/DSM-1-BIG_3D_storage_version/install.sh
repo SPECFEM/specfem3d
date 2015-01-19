aa005dce88510ddfba0085fb77e24c1e4cf571f6
Clément Durochat <c.durochat@gmail.com>
2015-01-19 14:58:54 +0100

    Organisation of DSM directory for Nobu modifs 4

--- utils/DSM_FOR_SPECFEM3D/DSM-1-BIG_3D_storage_version/install.sh
+++ utils/DSM_FOR_SPECFEM3D/DSM-1-BIG_3D_storage_version/install.sh
@@ -7,7 +7,7 @@ neutre='\e[0;m'
 
 echo " "
 
-HOME_DSM_MAIN_DIR=/ccc/scratch/cont003/gen7165/durochtc/Codes/SPECFEM3Ds/specfem3d/utils/DSM_FOR_SPECFEM3D
+HOME_DSM_MAIN_DIR=/ccc/scratch/cont003/gen7165/durochtc/Codes/SPECFEM3Ds/specfem3d/utils/DSM_FOR_SPECFEM3D/
 export HOME_DSM_MAIN_DIR
 
 echo -e "!! CAUTION !! Verify the definition of your ${jaune}\033[1mHOME_DSM_MAIN_DIR\033[0m${neutre} in install.sh, currently defined as: "
@@ -26,29 +26,31 @@ echo " "
 echo "The mesher part is now generated in meshfem3d (as a particular case)"
 
 echo " "
-cd ${HOME_DSM_MAIN_DIR}/Part-1a-compute_DSM_coefficients_with_FEMs_SH/ ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-1a-compute_DSM_coefficients_with_FEMs_SH/ ; make
 
-cd ${HOME_DSM_MAIN_DIR}/Part-1b-compute_DSM_coefficients_with_FEMs_PSV/ ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-1b-compute_DSM_coefficients_with_FEMs_PSV/ ; make
 
-cd ${HOME_DSM_MAIN_DIR}/Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_vertical_faces/ ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_Zmin/ ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_vertical_faces/ ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-2a-read_DSM_coefficients_back_SH/TraPSV_MPI_read_Zmin/ ; make
 
-cd ${HOME_DSM_MAIN_DIR}/Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_vertical_faces/ ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_Zmin/ ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_vertical_faces/ ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-2b-read_DSM_coefficients_back_PSV/TraPSV_MPI_read_Zmin/ ; make
 
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_SH ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_SH ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_PSV ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_PSV ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_FULL ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_FULL ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin_disp ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_disp ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat ; make
-cd ${HOME_DSM_MAIN_DIR}/Part-3-modify_DSM_results_for_SPECFEM/Interf_SPECFEM3D_DSM ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_SH ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_SH ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_PSV ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_PSV ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_ZMIN_FULL ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/FFT_MPI_FACES_VERT_FULL ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin_disp ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_zmin ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat_disp ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/ChangeFormat ; make
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/Part-3-modify_DSM_results_for_SPECFEM/Interf_SPECFEM3D_DSM ; make
 
-cd ${HOME_DSM_MAIN_DIR}/UTILS/ ; make 
+cd ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/UTILS/ ; make 
 
-cd ${HOME_DSM_MAIN_DIR}/
+mv ${HOME_DSM_MAIN_DIR}/DSM-1-BIG_3D_storage_version/bin ${HOME_DSM_MAIN_DIR}
+
+cd ${HOME_DSM_MAIN_DIR}
 
