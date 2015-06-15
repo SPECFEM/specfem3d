a6bc9bff662e230b1ab7ea5ba241827ec16874b0
Clément Durochat <c.durochat@gmail.com>
2015-06-15 12:26:26 +0200

    Add script files for batch submission and automatic concatenation

--- utils/EXTERNAL_CODES_coupled_with_SPECFEM3D/AxiSEM_for_SPECFEM3D/scripts_SPECFEM3D_for_AxiSEM.sh
+++ utils/EXTERNAL_CODES_coupled_with_SPECFEM3D/AxiSEM_for_SPECFEM3D/scripts_SPECFEM3D_for_AxiSEM.sh
@@ -25,6 +25,8 @@ cp $AxiSEM_FILE_1D_MODEL MESH/.
 
 $SPECFEM3D_BINARY_PATH/xmeshfem3D > Step1-create_3D_chunk_mesh.out
 
+cp list_ggl_boundary_* ${HOME_AxiSEM}/AxiSEM_modif_for_coupling_with_specfem/SOLVER/
+
 }
 
 function run_create_partitioning_and_specfem_databases ()
