c59e613fbbcd9f98b1de9c05c188db2911f08f0c
xiezhinan <xiezhinan1984@gmail.com>
2015-06-23 09:19:47 +0800

    add one comment in get_force.f90 in order to facilitate the older user who still use the old FORCESOLUTION file to find out why the code stop with error forrtl: severe (59): list-directed I/O syntax error, unit -5, file Internal List-Directed Read

--- src/shared/get_force.f90
+++ src/shared/get_force.f90
@@ -101,7 +101,7 @@
 
     ! read f0 (stored in hdur() array for convenience, to use the same array as for CMTSOLUTION)
     ! Please be careful, if you meet an error in reading the file FORCESOLUTION,
-    ! such as you still write “hdur:” instead of “f0:”
+    ! such as you still write "hdur:" instead of "f0:"
     ! Please change your file or do following change in the code, such as changing 
     ! read(string(4:len_trim(string)),*) hdur(isource)
     ! to 
