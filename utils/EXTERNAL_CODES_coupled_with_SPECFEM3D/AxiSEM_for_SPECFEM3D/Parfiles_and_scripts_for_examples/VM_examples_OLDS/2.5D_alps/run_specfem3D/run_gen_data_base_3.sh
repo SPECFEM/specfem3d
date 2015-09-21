4600ae7f000749779ba372021c258aa2bd779fe8
Clément Durochat <c.durochat@gmail.com>
2015-09-21 15:17:11 +0200

    Delete useless files

--- utils/EXTERNAL_CODES_coupled_with_SPECFEM3D/AxiSEM_for_SPECFEM3D/Parfiles_and_scripts_for_examples/VM_examples_OLDS/2.5D_alps/run_specfem3D/run_gen_data_base_3.sh
+++
@@ -1,5 +0,0 @@
-source ./set_path_and_params.sh
-
-
-cp MESH/model_1D.in DATA/.  # important copy the file produced by mesher
-mpirun -np $NPROC $SEMBIN/xgenerate_databases
--- utils/EXTERNAL_CODES_coupled_with_SPECFEM3D/AxiSEM_for_SPECFEM3D/Parfiles_and_scripts_for_examples/VM_examples_OLDS/2.5D_alps/run_specfem3D/set_path_and_params.sh
+++
@@ -1,2 +0,0 @@
-NPROC=8
-SEMBIN=/home/monteill/progs/GIT/specfem3d_en_cours_de_modif/bin/
