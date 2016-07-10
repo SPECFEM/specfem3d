270bc57041b99c95d30bdd9d65e199e6e34d092c
daniel peter <daniel.peter22@gmail.com>
2016-07-10 10:18:47 +0300

    updates run script in homogeneous_halfspace_HEX8_elastic_no_absorbing/

--- EXAMPLES/homogeneous_halfspace_HEX8_elastic_no_absorbing/run_this_example.sh
+++ EXAMPLES/homogeneous_halfspace_HEX8_elastic_no_absorbing/run_this_example.sh
@@ -1,51 +1,24 @@
 #!/bin/bash
-#
-# script runs decomposition,database generation and solver
-# using this example setup
-#
-# prior to running this script, you must create the mesh files
-# in directory MESH/
-# (see section 3.1 "Meshing with CUBIT" in user guide)
-#
-
-###################################################
-
-# number of processes
-NPROC=4
-
-##################################################
 
 echo "running example: `date`"
 currentdir=`pwd`
-
-echo
-echo "(will take about 5 minutes)"
-echo
 
 # sets up directory structure in current example directory
 echo
 echo "   setting up example..."
 echo
 
-mkdir -p bin
-mkdir -p OUTPUT_FILES/DATABASES_MPI
-
-rm -f OUTPUT_FILES/*
-rm -rf OUTPUT_FILES/DATABASES_MPI/*
-
-# compiles executables in root directory
-cd ../../
-
-rm -fr DATA/*
-cd $currentdir
-cp -fr DATA/* ../../DATA/.
+# cleans output files
+mkdir -p OUTPUT_FILES
+rm -rf OUTPUT_FILES/*
 
 # links executables
+mkdir -p bin
 cd bin/
 rm -f *
-cp ../../../bin/xdecompose_mesh .
-cp ../../../bin/xgenerate_databases .
-cp ../../../bin/xspecfem3D .
+ln -s ../../../bin/xdecompose_mesh
+ln -s ../../../bin/xgenerate_databases
+ln -s ../../../bin/xspecfem3D
 cd ../
 
 # stores setup
@@ -53,28 +26,58 @@ cp DATA/Par_file OUTPUT_FILES/
 cp DATA/CMTSOLUTION OUTPUT_FILES/
 cp DATA/STATIONS OUTPUT_FILES/
 
-# decomposes mesh
+# get the number of processors, ignoring comments in the Par_file
+NPROC=`grep ^NPROC DATA/Par_file | grep -v -E '^[[:space:]]*#' | cut -d = -f 2`
+
+BASEMPIDIR=`grep ^LOCAL_PATH DATA/Par_file | cut -d = -f 2 `
+mkdir -p $BASEMPIDIR
+
+# decomposes mesh using the pre-saved mesh files in MESH-default
 echo
 echo "  decomposing mesh..."
 echo
-./bin/xdecompose_mesh $NPROC ./MESH-default ./OUTPUT_FILES/DATABASES_MPI/
+./bin/xdecompose_mesh $NPROC ./MESH-default $BASEMPIDIR
+# checks exit code
+if [[ $? -ne 0 ]]; then exit 1; fi
 
 # runs database generation
-echo
-echo "  running database generation..."
-echo
-mpirun -np $NPROC ./bin/xgenerate_databases
+if [ "$NPROC" -eq 1 ]; then
+  # This is a serial simulation
+  echo
+  echo "  running database generation..."
+  echo
+  ./bin/xgenerate_databases
+else
+  # This is a MPI simulation
+  echo
+  echo "  running database generation on $NPROC processors..."
+  echo
+  mpirun -np $NPROC ./bin/xgenerate_databases
+fi
+# checks exit code
+if [[ $? -ne 0 ]]; then exit 1; fi
 
 # runs simulation
-echo
-echo "  running solver..."
-echo
-mpirun -np $NPROC ./bin/xspecfem3D
+if [ "$NPROC" -eq 1 ]; then
+  # This is a serial simulation
+  echo
+  echo "  running solver..."
+  echo
+  ./bin/xspecfem3D
+else
+  # This is a MPI simulation
+  echo
+  echo "  running solver on $NPROC processors..."
+  echo
+  mpirun -np $NPROC ./bin/xspecfem3D
+fi
+# checks exit code
+if [[ $? -ne 0 ]]; then exit 1; fi
 
 echo
 echo "see results in directory: OUTPUT_FILES/"
 echo
 echo "done"
-date
+echo `date`
 
 
