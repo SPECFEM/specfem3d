20d81aedf7b8424477d4962be5b8bd7ebd0d624e
Kangchen Bai <kbai@login-5-2.local>
2016-08-09 16:58:23 -0700

    Add reference output for 2 fault exapmles.

---
+++ EXAMPLES/fault_examples/tpv103/REF_OUTPUT/compare_results.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sys
+
+def usage():
+    print "compare_results filename1 filename2"
+ 
+#print len(sys.argv)
+if len(sys.argv) != 3:
+    usage()
+dat = np.loadtxt(sys.argv[1], skiprows = 21)
+dat2= np.loadtxt(sys.argv[2], skiprows = 21)
+
+   
