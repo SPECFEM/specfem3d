10d3f3b595a15638309cd2da3c44d16bec33d29b
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2017-02-27 01:22:07 +0100

    removed an unused variable

--- src/specfem3D/prepare_timerun.F90
+++ src/specfem3D/prepare_timerun.F90
@@ -822,7 +822,7 @@
   subroutine prepare_timerun_pml()
 
   use pml_par
-  use specfem_par, only: NGNOD,myrank,SIMULATION_TYPE,GPU_MODE
+  use specfem_par, only: myrank,SIMULATION_TYPE,GPU_MODE
   use constants, only: IMAIN,NGNOD_EIGHT_CORNERS
 
   implicit none
