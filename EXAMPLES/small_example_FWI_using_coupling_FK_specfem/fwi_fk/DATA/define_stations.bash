b6fe0ecca05ca9078ae81f466b5a82f0146dc482
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2017-09-08 03:33:12 +0200

    added examples of coupling with injection techniques

---
+++ EXAMPLES/small_example_FWI_using_coupling_FK_specfem/fwi_fk/DATA/define_stations.bash
@@ -0,0 +1,39 @@
+# create grid of stations
+# script to create station file for specfem
+# compile tools in the folowing directory :
+# (update compile.csh script, just fill the fortran compiler name)
+dir=../../../../EXTERNAL_CODES_coupled_with_SPECFEM3D/AxiSEM_for_SPECFEM3D/utils_misc/
+
+
+# numbers of stations
+nb_sta_x=29
+nb_sta_y=29
+
+# distance betwee station (m)
+distance_betxeen_sta_x=1000
+distance_betxeen_sta_Y=1000
+
+# coordinate of first station (m)
+xmin=2000
+ymin=2000
+
+# station depth
+depth=0
+
+#
+NETWORK=XX
+CODE=S
+
+echo $nb_sta_x $nb_sta_y > Par_sta
+echo $distance_betxeen_sta_x $distance_betxeen_sta_Y >> Par_sta
+echo $xmin $ymin >> Par_sta
+echo $depth >> Par_sta
+echo $CODE >> Par_sta
+echo $NETWORK >> Par_sta
+
+$dir/create_uniform_stations.x<<EOF
+Par_sta
+EOF
+
+
+cp station_list.txt STATIONS
