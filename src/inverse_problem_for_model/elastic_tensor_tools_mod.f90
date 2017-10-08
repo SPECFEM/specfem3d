bdf7dd3443f7a1ccb034385afc444a9224ef3823
Stephen Beller <beller.stephen@gmail.com>
2017-10-08 03:07:28 +0200

    add two new module for anisotropic full-waveform inversion in tti media and elastic tensor manipulation

---
+++ src/inverse_problem_for_model/elastic_tensor_tools_mod.f90
@@ -0,0 +1,54 @@
+module elastic_tensor_tools_mod
+  
+  use interpolation_mod, only: si, sp, di, dp, cp, hp
+
+  implicit none
+
+contains
+
+  subroutine define_rotation_matrix
+
+
+  end subroutine define_rotation_matrix
+
+  subroutine rotate_fourth_order_tensor
+
+
+  end subroutine rotate_fourth_order_tensor
+
+  subroutine get_dilatational_stiffness_tensor
+
+
+  end subroutine get_dilatational_stiffness_tensor
+
+  subroutine get_voigt_stiffness_tensor
+
+
+  end subroutine get_voigt_stiffness_tensor
+
+  subroutine elastic_tensor_to_elastic_vector
+
+
+  end subroutine elastic_tensor_to_elastic_vector
+
+  subroutine elastic_vector_to_elastic_tensor
+
+
+  end subroutine elastic_vector_to_elastic_tensor
+  
+  subroutine get_isotropic_part_fedorov
+
+
+  end subroutine get_isotropic_part_fedorov
+
+  subroutine define_isotropic_tensor
+
+
+  end subroutine define_isotropic_tensor
+
+  subroutine define_hexagonal_tensor
+
+
+  end subroutine define_hexagonal_tensor
+  
+end module elastic_tensor_tools_mod
