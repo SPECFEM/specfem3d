f822df6e72e1f6bd8855949f6c54916eba658fc3
Dimitri Komatitsch <komatitsch@lma.cnrs-mrs.fr>
2017-10-31 00:57:27 +0100

    improved EXAMPLES/change_something_in_all_the_Par_files_automatically.bash

--- EXAMPLES/change_something_in_all_the_Par_files_automatically.bash
+++ EXAMPLES/change_something_in_all_the_Par_files_automatically.bash
@@ -4,5 +4,7 @@
 # If you want to change the name of a parameter or anything else in all the Par_files in an automatic way
 # you can also use a "find" command combined with a "sed" command, here is an example:
 
-find . -type f -name "*Par_file*" -exec sed -i 's/ATTENUATION_VISCOELASTIC_SOLID/ATTENUATION_VISCOELASTIC/g' {} \;
+#  [ ]*  below is very useful, it matches any number of white spaces, thus makes the search insensitive to where the = sign is located in the initial line
+
+find . -type f -name "*Par_file*" -exec sed -i 's/ATTENUATION_VISCOELASTIC_SOLID[ ]*=/ATTENUATION_VISCOELASTIC    =/g' {} \;
 
