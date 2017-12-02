d3b5abf2b7401e2545a047cc185620dd10f5beb9
Stephen Beller <beller.stephen@gmail.com>
2017-12-02 15:47:46 +0100

    remove flag, already defined in constants

--- src/specfem3D/specfem3D_par.F90
+++ src/specfem3D/specfem3D_par.F90
@@ -474,10 +474,6 @@ module specfem_par_acoustic
   ! LDDRK time scheme
   real(kind=CUSTOM_REAL), dimension(:), allocatable :: potential_acoustic_lddrk,potential_dot_acoustic_lddrk
 
-  ! FLAG for Dipole source in fluid
-  logical :: DIPOLE_SOURCE_IN_FLUID
-
-
 end module specfem_par_acoustic
 
 !=====================================================================
