916fbdaa13ec3559e38246042f9466f4b9643dda
Daniel Peter <daniel.peter22@gmail.com>
2018-01-16 23:28:42 +0300

    updates hexahedral face detection routine

--- src/shared/get_element_face.f90
+++ src/shared/get_element_face.f90
@@ -36,20 +36,25 @@
 
   implicit none
 
-  integer :: ispec,nspec,nglob,iface_id
+  integer,intent(in) :: ispec,nspec,nglob
+  integer,intent(out) :: iface_id
 
 ! face corner locations
-  real(kind=CUSTOM_REAL),dimension(NGNOD2D_FOUR_CORNERS) :: xcoord,ycoord,zcoord
+  real(kind=CUSTOM_REAL),dimension(NGNOD2D_FOUR_CORNERS),intent(in) :: xcoord,ycoord,zcoord
 
 ! index array
-  integer, dimension(NGLLX,NGLLY,NGLLZ,nspec) :: ibool
+  integer, dimension(NGLLX,NGLLY,NGLLZ,nspec),intent(in) :: ibool
 
 ! global point locations
-  real(kind=CUSTOM_REAL) :: xstore_dummy(nglob),ystore_dummy(nglob),zstore_dummy(nglob)
+  real(kind=CUSTOM_REAL),intent(in) :: xstore_dummy(nglob),ystore_dummy(nglob),zstore_dummy(nglob)
 
 ! local parameters
-  real(kind=CUSTOM_REAL),dimension(NGNOD2D_FOUR_CORNERS) :: xcoord_face,ycoord_face,zcoord_face
+  real(kind=CUSTOM_REAL) :: xcoord_face,ycoord_face,zcoord_face
   real(kind=CUSTOM_REAL) :: midpoint_faces(NDIM,6),midpoint(NDIM),midpoint_distances(6)
+  real(kind=CUSTOM_REAL) :: edge_length,avg_factor
+
+  ! distance tolerance (relative to coordinate range)
+  real(kind=CUSTOM_REAL), parameter :: TOL_DIST = 1.e-4
 
 ! corners indices of reference cube faces
   ! shapes of arrays below
@@ -85,19 +90,19 @@
 
 ! initializes
   iface_id = -1
+  avg_factor = 1.0_CUSTOM_REAL / NGNOD2D_FOUR_CORNERS
 
 ! gets face midpoint by its corners
   midpoint(:) = 0.0
-  do icorner=1,NGNOD2D_FOUR_CORNERS
-    midpoint(1) = midpoint(1) + xcoord(icorner)
-    midpoint(2) = midpoint(2) + ycoord(icorner)
-    midpoint(3) = midpoint(3) + zcoord(icorner)
+  do icorner = 1,NGNOD2D_FOUR_CORNERS
+    midpoint(1) = midpoint(1) + xcoord(icorner) * avg_factor
+    midpoint(2) = midpoint(2) + ycoord(icorner) * avg_factor
+    midpoint(3) = midpoint(3) + zcoord(icorner) * avg_factor
   enddo
-  midpoint(:) = midpoint(:) / 4.0
 
 ! determines element face by minimum distance of midpoints
   midpoint_faces(:,:) = 0.0
-  do ifa=1,6
+  do ifa = 1,6
 
     ! face corners
     do icorner = 1,NGNOD2D_FOUR_CORNERS
@@ -107,34 +112,34 @@
 
       ! coordinates
       iglob = ibool(i,j,k,ispec)
-      xcoord_face(icorner) = xstore_dummy(iglob)
-      ycoord_face(icorner) = ystore_dummy(iglob)
-      zcoord_face(icorner) = zstore_dummy(iglob)
+      xcoord_face = xstore_dummy(iglob)
+      ycoord_face = ystore_dummy(iglob)
+      zcoord_face = zstore_dummy(iglob)
 
       ! face midpoint coordinates
-      midpoint_faces(1,ifa) =  midpoint_faces(1,ifa) + xcoord_face(icorner)
-      midpoint_faces(2,ifa) =  midpoint_faces(2,ifa) + ycoord_face(icorner)
-      midpoint_faces(3,ifa) =  midpoint_faces(3,ifa) + zcoord_face(icorner)
+      midpoint_faces(1,ifa) =  midpoint_faces(1,ifa) + xcoord_face * avg_factor
+      midpoint_faces(2,ifa) =  midpoint_faces(2,ifa) + ycoord_face * avg_factor
+      midpoint_faces(3,ifa) =  midpoint_faces(3,ifa) + zcoord_face * avg_factor
     enddo
 
-    midpoint_faces(:,ifa) = midpoint_faces(:,ifa) / 4.0
-
-    ! distance
+    ! distance squared
     midpoint_distances(ifa) = (midpoint(1)-midpoint_faces(1,ifa))**2 &
                             + (midpoint(2)-midpoint_faces(2,ifa))**2 &
                             + (midpoint(3)-midpoint_faces(3,ifa))**2
   enddo
 
-! gets closest point, which determines face
+  ! gets closest point, which determines face
   iloc = minloc(midpoint_distances)
 
   ! checks that found midpoint is close enough
-  if (midpoint_distances(iloc(1)) > 1.e-4 * &
-          (   (xcoord(1)-xcoord(2))**2 &
-            + (ycoord(1)-ycoord(2))**2 &
-            + (zcoord(1)-zcoord(2))**2)) then
+  !
+  ! note: distance is relative. depending on the mesh, coordinate points might vary on very small (<1mm) or large (>1km) scales.
+  !       the check here takes the first edge of the surface element as a proxy for the scale
+  edge_length = (xcoord(1)-xcoord(2))**2 + (ycoord(1)-ycoord(2))**2 + (zcoord(1)-zcoord(2))**2
+
+  if (midpoint_distances(iloc(1)) > TOL_DIST * edge_length) then
     print *,'error element face midpoint distance:',midpoint_distances(iloc(1)), &
-          (xcoord(1)-xcoord(2))**2+(ycoord(1)-ycoord(2))**2+(zcoord(1)-zcoord(2))**2
+            ' - edge length:',edge_length,' tolerance:',TOL_DIST*edge_length
     ! corner locations
     do icorner = 1,NGNOD2D_FOUR_CORNERS
       i = iface_all_corner_ijk(1,icorner,iloc(1))
@@ -316,6 +321,7 @@ end subroutine get_element_face_gll_indices
   face_n(1) =   (ycoord(2)-ycoord(1))*(zcoord(3)-zcoord(1)) - (zcoord(2)-zcoord(1))*(ycoord(3)-ycoord(1))
   face_n(2) = - (xcoord(2)-xcoord(1))*(zcoord(3)-zcoord(1)) + (zcoord(2)-zcoord(1))*(xcoord(3)-xcoord(1))
   face_n(3) =   (xcoord(2)-xcoord(1))*(ycoord(3)-ycoord(1)) - (ycoord(2)-ycoord(1))*(xcoord(3)-xcoord(1))
+
   tmp = sqrt( face_n(1)*face_n(1) + face_n(2)*face_n(2) + face_n(3)*face_n(3) )
   if (abs(tmp) < TINYVAL) then
     print *,'error get face normal: length',tmp
@@ -425,6 +431,7 @@ end subroutine get_element_face_gll_indices
   face_n(1) =   (ycoord(2)-ycoord(1))*(zcoord(3)-zcoord(1)) - (zcoord(2)-zcoord(1))*(ycoord(3)-ycoord(1))
   face_n(2) = - (xcoord(2)-xcoord(1))*(zcoord(3)-zcoord(1)) + (zcoord(2)-zcoord(1))*(xcoord(3)-xcoord(1))
   face_n(3) =   (xcoord(2)-xcoord(1))*(ycoord(3)-ycoord(1)) - (ycoord(2)-ycoord(1))*(xcoord(3)-xcoord(1))
+
   tmp = sqrt( face_n(1)**2 + face_n(2)**2 + face_n(3)**2)
   if (abs(tmp) < TINYVAL) then
     print *,'error get face normal: length',tmp
