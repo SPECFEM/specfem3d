c96ab765c4d8a3148a4199d2624fa765136a229a
mnagaso <mnsaru22@gmail.com>
2018-08-29 22:00:20 +0200

    code and sample for combining seismo signals

---
+++ utils/combine_result_singals_to_one_hdf5/test_script_convert_signals_to_single_binary.py
@@ -0,0 +1,22 @@
+import subprocess
+
+def main():
+    """
+    """
+
+    file_of_signal_list = "list_signal_files.txt" # path to a file which is a list of pathes to .semp files
+    output_file_name    = "test_out.h5"           # name of output binary file
+    total_timesteps     = 5000                    # total number of timesteps in your calculation
+    total_num_sigs      = 5                       # total number of signals (.semp files) to be combined and binarized
+
+    # time window for limited extraction of timesteps
+    time_window = [0,total_timesteps-1]  
+
+    # binarize_signals
+    cmd = './combine ' + file_of_signal_list + ' ' + output_file_name + ' ' + str(total_num_sigs) + \
+                   ' ' + str(time_window[0]) + ' ' +  str(time_window[1]) + ' ' + str(total_timesteps)
+    subprocess.call(cmd, shell=True)
+
+
+if __name__ == '__main__':
+    main()   
