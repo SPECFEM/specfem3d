05c54a8580b5024ccee30c8f215201272a509704
Daniel Peter <daniel.peter22@gmail.com>
2021-04-19 17:40:21 +0300

    updates to avoid compiler issue

--- src/generate_databases/finalize_databases.f90
+++ src/generate_databases/finalize_databases.f90
@@ -39,7 +39,7 @@
   integer :: nspec_total
   ! this can overflow if more than 2 Gigapoints in the whole mesh, thus replaced with double precision version
   integer(kind=8) :: nglob_total
-  double precision :: nglob_local,nglob_total_db
+  double precision :: nglob_l,nglob_total_db
 
   ! timing
   double precision, external :: wtime
@@ -48,12 +48,16 @@
   call sum_all_i(NSPEC_AB,nspec_total)
 
   ! this can overflow if more than 2 Gigapoints in the whole mesh, thus replaced with double precision version
-  nglob_local = dble(NGLOB_AB)
-  call sum_all_dp(nglob_local,nglob_total_db)
-  nglob_total = int(nglob_total_db,kind=8)
+  nglob_l = dble(NGLOB_AB)
+  call sum_all_dp(nglob_l,nglob_total_db)
 
-  call synchronize_all()
+  ! user output
   if (myrank == 0) then
+    ! converts to integer*8
+    ! note: only the main process has the total sum in nglob_total_db and a valid value;
+    !       this conversion could lead to compiler errors if done by other processes.
+    nglob_total = int(nglob_total_db,kind=8)
+
     write(IMAIN,*)
     write(IMAIN,*) 'Repartition of elements:'
     write(IMAIN,*) '-----------------------'
@@ -78,16 +82,20 @@
     write(IMAIN,*)
     write(IMAIN,*) 'smallest and largest possible floating-point numbers are: ',tiny(1._CUSTOM_REAL),huge(1._CUSTOM_REAL)
     write(IMAIN,*)
+    call flush_IMAIN()
   endif
 
-! copy number of elements and points in an include file for the solver
+  ! synchronizes processes
+  call synchronize_all()
+
+  ! copy number of elements and points in an include file for the solver
   if (myrank == 0) then
     call save_header_file(NSPEC_AB,NGLOB_AB,NPROC, &
                           ATTENUATION,ANISOTROPY,NSTEP,DT,STACEY_INSTEAD_OF_FREE_SURFACE, &
                           SIMULATION_TYPE,max_memory_size,nfaces_surface_glob_ext_mesh)
   endif
 
-! elapsed time since beginning of mesh generation
+  ! elapsed time since beginning of mesh generation
   if (myrank == 0) then
     tCPU = wtime() - time_start
     write(IMAIN,*)
@@ -96,14 +104,14 @@
     write(IMAIN,*)
   endif
 
-! close main output file
+  ! close main output file
   if (myrank == 0) then
     write(IMAIN,*) 'done'
     write(IMAIN,*)
     close(IMAIN)
   endif
 
-! synchronize all the processes to make sure everybody has finished
+  ! synchronize all the processes to make sure everybody has finished
   call synchronize_all()
 
   end subroutine finalize_databases
