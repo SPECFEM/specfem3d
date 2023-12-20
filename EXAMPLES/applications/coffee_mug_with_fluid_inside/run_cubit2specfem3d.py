0e783f51a752e2630f43d5092854df4a1f42f876
Daniel Peter <daniel.b.peter@gmail.com>
2023-12-20 11:59:33 +0100

    updates python script (reload)

--- EXAMPLES/applications/coffee_mug_with_fluid_inside/run_cubit2specfem3d.py
+++ EXAMPLES/applications/coffee_mug_with_fluid_inside/run_cubit2specfem3d.py
@@ -27,7 +27,8 @@ print("")
 
 ###### This is boundary_definition.py of GEOCUBIT
 #..... which extracts the bounding faces and defines them into blocks
-#reload(boundary_definition)
+#import importlib
+#importlib.(boundary_definition)
 #boundary_definition.entities=['face']
 #boundary_definition.define_bc(boundary_definition.entities,parallel=True)
 
@@ -35,7 +36,9 @@ print("")
 #### Export to SESAME format using cubit2specfem3d.py of GEOCUBIT
 os.system('mkdir -p MESH')
 
-reload(cubit2specfem3d)
+import importlib
+importlib.reload(cubit2specfem3d)
+
 cubit2specfem3d.export2SPECFEM3D('MESH')
 
 # all files needed by SCOTCH are now in directory MESH
